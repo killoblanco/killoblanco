eeaac144233cd88effd38d5fa9909f140e0826d9
Kamilo Vasquez <killo.blanco@gmail.com>
2023-11-16 22:12:37 -0500

    🌐 Fix formatting in Spanish landing page content

--- src/i18n/landing/es.ts
+++ src/i18n/landing/es.ts
@@ -9,7 +9,7 @@ export const es = {
     title: 'Sobre Mi',
     content: [
       '¡Saludos! Soy Kamilo Vasquez, un apasionado desarrollador de software con más de una década de experiencia desde 2010. Mi misión es crear soluciones tecnológicas innovadoras que impacten positivamente la vida de las personas.',
-      'A lo largo de mi carrera, he colaborado con equipos internacionales en proyectos de gran envergadura, asumiendo roles clave en el desarrollo web.Mi versatilidad se extiende más allá del desarrollo web, abarcando también aplicaciones móviles y de escritorio.Mi enfoque autodidacta me impulsa constantemente a explorar nuevas tecnologías y estoy particularmente entusiasmado con el mundo de la Inteligencia Artificial.'
+      'A lo largo de mi carrera, he colaborado con equipos internacionales en proyectos de gran envergadura, asumiendo roles clave en el desarrollo web. Mi versatilidad se extiende más allá del desarrollo web, abarcando también aplicaciones móviles y de escritorio. Mi enfoque autodidacta me impulsa constantemente a explorar nuevas tecnologías y estoy particularmente entusiasmado con el mundo de la Inteligencia Artificial.'
     ]
   }
 }
