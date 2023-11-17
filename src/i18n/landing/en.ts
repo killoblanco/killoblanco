c78d7bc1d408776d8e9f1e6f0d50196ceaefff94
Kamilo Vasquez <killo.blanco@gmail.com>
2023-11-17 00:48:17 -0500

    🌐 Add skills section to landing page in English and Spanish

--- src/i18n/landing/en.ts
+++ src/i18n/landing/en.ts
@@ -11,5 +11,9 @@ export const en = {
       "Greetings! I am Kamilo Vasquez, a passionate software developer with over a decade of experience since 2010. My mission is to create innovative technology solutions that positively impact people's lives.",
       "Throughout my career, I have collaborated with international teams on large projects, assuming key roles in web development. My versatility extends beyond web development, also encompassing mobile and desktop applications. My self-taught approach constantly drives me to explore new technologies and I am particularly excited about the world of Artificial Intelligence."
     ]
+  },
+  skills: {
+    title: 'Skills & Technologies',
+    content: 'Infrastructure and multiplatform full-stack applications builder with cutting-edge technologies.'
   }
 }
