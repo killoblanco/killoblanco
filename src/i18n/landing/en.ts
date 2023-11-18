c7598f3e0d474c15bef83882ccb9e9e4df1cff28
Kamilo Vasquez <killo.blanco@gmail.com>
2023-11-18 01:34:38 -0500

    🌐 Update about me content

--- src/i18n/landing/en.ts
+++ src/i18n/landing/en.ts
@@ -8,7 +8,7 @@ export const en = {
   about: {
     title: 'About Me',
     content: [
-      "Greetings! I am Kamilo Vasquez, a passionate software developer with over a decade of experience since 2010. My mission is to create innovative technology solutions that positively impact people's lives.",
+      "I am Kamilo Vasquez, a passionate software developer with over a decade of experience since 2010. My mission is to create innovative technology solutions that positively impact people's lives.",
       "Throughout my career, I have collaborated with international teams on large projects, assuming key roles in web development. My versatility extends beyond web development, also encompassing mobile and desktop applications. My self-taught approach constantly drives me to explore new technologies and I am particularly excited about the world of Artificial Intelligence."
     ]
   },
--- src/i18n/landing/es.ts
+++ src/i18n/landing/es.ts
@@ -8,7 +8,7 @@ export const es = {
   about: {
     title: 'Acerca de mí',
     content: [
-      '¡Saludos! Soy Kamilo Vasquez, un apasionado desarrollador de software con más de una década de experiencia desde 2010. Mi misión es crear soluciones tecnológicas innovadoras que impacten positivamente la vida de las personas.',
+      'Soy Kamilo Vasquez, un apasionado desarrollador de software con más de una década de experiencia desde 2010. Mi misión es crear soluciones tecnológicas innovadoras que impacten positivamente la vida de las personas.',
       'A lo largo de mi carrera, he colaborado con equipos internacionales en proyectos de gran envergadura, asumiendo roles clave en el desarrollo web. Mi versatilidad se extiende más allá del desarrollo web, abarcando también aplicaciones móviles y de escritorio. Mi enfoque autodidacta me impulsa constantemente a explorar nuevas tecnologías y estoy particularmente entusiasmado con el mundo de la Inteligencia Artificial.'
     ]
   },
