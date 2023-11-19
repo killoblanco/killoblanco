d156b67556d1cb9cb5e23f928301a918628f8929
Kamilo Vasquez <killo.blanco@gmail.com>
2023-11-19 10:54:29 -0500

    🔧 Add React integration to astro.config.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import tailwind from "@astrojs/tailwind";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   output: "server",
-  adapter: cloudflare(),
+  adapter: cloudflare()
 });
