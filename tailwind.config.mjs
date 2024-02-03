e5aaf674c02f33d8324bb8b9c4e89f99667989fb
Kamilo Vasquez <killo.blanco@gmail.com>
2024-02-03 01:19:38 -0500

    🔧 💄 Update tailwind.config.mjs with new colors and font families

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,58 +1,31 @@
 import typography from '@tailwindcss/typography'
-import daisyui from 'daisyui'
+import chroma from 'chroma-js'
 
-const themes = [{
-  light: {
-    primary: '#416917',
-    'primary-content': '#ffffff',
-    secondary: '#57624a',
-    'secondary-content': '#ffffff',
-    accent: '#386664',
-    'accent-content': '#ffffff',
-    neutral: '#fdfcf5',
-    'neutral-content': '#1b1c18',
-    'base-100': '#eff2e3',
-    'base-200': '#e0e4d5',
-    'base-300': '#c4c8ba',
-    'base-content': '#191d14',
-    info: '#0e5cb7',
-    'info-content': '#ffffff',
-    success: '#4e6700',
-    'success-content': '#ffffff',
-    warning: '#7a5900',
-    'warning-content': '#ffffff',
-    error: '#ba1a1a',
-    'error-content': '#ffffff',
-  },
-  dark: {
-    primary: '#a5d576',
-    'primary-content': '#1c3700',
-    secondary: '#bfcbad',
-    'secondary-content': '#29331f',
-    accent: '#a0cfcc',
-    'accent-content': '#003735',
-    neutral: '#1b1c18',
-    'neutral-content': '#e3e3dc',
-    'base-100': '#191d14',
-    'base-200': '#2e3228',
-    'base-300': '#393d33',
-    'base-content': '#fafeef',
-    info: '#abc7ff',
-    'info-content': '#002f66',
-    success: '#add543',
-    'success-content': '#273500',
-    warning: '#fabd24',
-    'warning-content': '#402d00',
-    error: '#ffb4ab',
-    'error-content': '#690005',
-  },
-}]
+const colors = [
+  ['neutral', '#383a36'],
+  ['brand', '#1DE9B6'],
+].reduce((acc, [name, color]) => {
+  const shades = chroma
+  .scale(['black', color, 'white'])
+  .mode('hsl')
+  .correctLightness()
+  .colors(26, 'hex')
+
+  return {
+    ...acc,
+    [name]: shades.reduce((acc, shade, idx) => ({
+      ...acc,
+      [idx * 4]: shade,
+    }), {})
+  }
+}, {});
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      colors,
       screens: {
         sm: '480px',
         md: '720px',
@@ -60,17 +33,12 @@ export default {
         xl: '1200px',
       },
       fontFamily: {
-        sans: ['Readex Pro Variable', 'sans-serif'],
-        serif: ['Readex Pro Variable', 'serif'],
+        sans: ['Outfit Variable', 'system-ui', 'sans-serif'],
+        serif: ['Outfit Variable', 'system-ui', 'serif'],
       },
     },
   },
   plugins: [
     typography({ className: 'typo' }),
-    daisyui
   ],
-  daisyui: {
-    themes,
-    darkTheme: 'dark',
-  }
 }
