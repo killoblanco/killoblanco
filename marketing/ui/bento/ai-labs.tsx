d5c1b9354101f1fac90c279c4dd8df2ca1d69acb
kamilo.vasquez <kamilo.vasquez.-nd@disney.com>
2024-04-13 00:50:21 -0500

    ✨ Add new BentoShow marketing ui component

---
+++ marketing/ui/bento/ai-labs.tsx
@@ -0,0 +1,16 @@
+import { Card } from "@/ui/elements/card";
+
+const IALabsCard = () => (
+  <Card className="bg-gradient-to-tl from-zinc-900 to-zinc-600 text-white p-4 col-start-1 col-end-3 row-start-1 row-end-3 grid grid-cols-3 gap-4 items-center ">
+    <h3 className="scroll-m-20 text-3xl font-semibold tracking-tight col-span-2">
+      Proyectos de
+      <br />
+      <span className="bg-gradient-to-r from-amber-300 to-amber-500 bg-clip-text text-transparent">
+        Inteligencia Artificial
+      </span>
+    </h3>
+    <span className="icon-[fluent-emoji--magic-wand] size-24 justify-self-center"/>
+  </Card>
+)
+
+export default IALabsCard;
---
+++ marketing/ui/bento/index.tsx
@@ -0,0 +1,9 @@
+import IALabsCard from "./ai-labs";
+
+const BentoShow = () => (
+  <div className="grid grid-cols-4 grid-rows-6 gap-8">
+    <IALabsCard />
+  </div>
+)
+
+export default BentoShow;
