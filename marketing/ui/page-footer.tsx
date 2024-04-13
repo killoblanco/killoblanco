34e61e0e296a5c7ba07c82234a69f3c8819c4fe6
kamilo.vasquez <kamilo.vasquez.-nd@disney.com>
2024-04-13 00:46:36 -0500

    ✨ Add new PageFooter marketing component

---
+++ marketing/ui/page-footer.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const PageFooter = () => (
+  <footer className="border-t border-t-border py-8 flex flex-col md:flex-row gap-4 items-center justify-between">
+    <Link href="#top" className="inline-flex gap-1 items-center text-sm text-muted-foreground">
+      <Image src="/avatar.webp" alt="Avatar" width={32} height={32} />
+      <span>Volver Arriba</span>
+      <span className="icon-[tabler--arrow-narrow-up] animate-bouncing repeat-infinite size-4" />
+    </Link>
+    <p className="text-sm text-muted-foreground inline-flex gap-1 items-center">
+      Hecho con
+      <span className="icon-[fluent-emoji--smiling-face-with-hearts] size-4" />
+      y
+      <span className="icon-[fluent-emoji--hot-beverage] size-4" />
+      por
+      <span className="font-medium">@killoblanco</span>
+    </p>
+  </footer>
+)
+export default PageFooter;
