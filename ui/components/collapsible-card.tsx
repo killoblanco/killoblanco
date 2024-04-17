76453bd2fc339c2d517734797e5123b2a1feb0da
kamilo.vasquez <kamilo.vasquez.-nd@disney.com>
2024-04-16 21:02:05 -0500

    ✨ Add CollapsibleCard component to handle collapsible cards in UI

---
+++ ui/components/collapsible-card.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { Card } from "@/ui/elements/card";
+import { Collapsible, CollapsibleContent } from "@/ui/elements/collapsible";
+import { CollapsibleTrigger } from "@radix-ui/react-collapsible";
+import { useState, type JSX, type PropsWithChildren } from 'react';
+
+type Props = {
+  title: JSX.Element
+  open?: boolean
+}
+
+export const CollapsibleCard = ({ title, open = false, children }: PropsWithChildren<Props>) => {
+  const [isOpen, setIsOpen] = useState(open);
+
+  return (
+    <Card className="p-4">
+      <Collapsible open={isOpen} onOpenChange={setIsOpen}>
+        <CollapsibleTrigger asChild>
+          {title}
+        </CollapsibleTrigger>
+        <CollapsibleContent className="mt-4 space-y-4">
+          {children}
+        </CollapsibleContent>
+      </Collapsible>
+    </Card>
+  )
+}
