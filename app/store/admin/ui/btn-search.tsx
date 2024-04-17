ad745b0cf8b5ce20e7539ae3928231694bd12e93
kamilo.vasquez <kamilo.vasquez.-nd@disney.com>
2024-04-16 21:00:32 -0500

    ✨ Add AdminBtnSearch component to handle search button in admin UI

---
+++ app/store/admin/ui/btn-search.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/ui/elements/button";
+
+const AdminBtnSearch = () => (
+  <Button variant="outline" className="w-60 gap-4 justify-between">
+    <span>Buscar en la tienda...</span>
+    <span className="icon-[tabler--search] size-4" />
+  </Button>
+)
+
+export default AdminBtnSearch;
