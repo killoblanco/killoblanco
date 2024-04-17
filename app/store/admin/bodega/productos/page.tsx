d711fd65dd9d8bd360679c917eaed929d677df1b
kamilo.vasquez <kamilo.vasquez.-nd@disney.com>
2024-04-16 22:20:28 -0500

    ✨ Add ProductsPage component

---
+++ app/store/admin/bodega/productos/page.tsx
@@ -0,0 +1,9 @@
+const ProductsPage = () => {
+  return (
+    <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight col-span-5 col-start-2">
+      Productos
+    </h3>
+  )
+}
+
+export default ProductsPage
