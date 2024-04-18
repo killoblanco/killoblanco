e5f343a3b6bb91b2daa42740b94fe4c267ab94b9
kamilo.vasquez <kamilo.vasquez.-nd@disney.com>
2024-04-18 03:04:28 -0500

    ✨ Add ProductDescriptionInfoGroup component to handle product description in UI

---
+++ app/store/admin/deposito/productos/forms/simple/components/description.tsx
@@ -0,0 +1,20 @@
+import { CollapsibleCard } from "@/ui/components/collapsible-card"
+import { TextEditorField } from "@/ui/form-fields/text-editor"
+
+export const ProductDescriptionInfoGroup = () => {
+  return (
+    <CollapsibleCard
+      title={(
+        <div className="flex items-center gap-4 w-full cursor-pointer">
+          <span className="icon-[tabler--file-text] size-6" />
+          <h4 className="scroll-m-20 text-xl font-semibold tracking-tight flex-1">
+            Descripción
+          </h4>
+        </div>
+
+      )}
+    >
+      <TextEditorField />
+    </CollapsibleCard>
+  )
+}
