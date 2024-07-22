c07caf8e4fe0c4d81f52c9b98c75eef9ebd3de0a
Kamilo Vasquez <killo.blanco@gmail.com>
2024-07-21 19:10:47 -0500

    ✨ New games btn

--- app/(marketing)/ui/sections/bento/index.tsx
+++ app/(marketing)/ui/sections/bento/index.tsx
@@ -1,8 +1,10 @@
 import ChatAi from './chat-ai'
+import Games from './games'
 
 const BentoShow = (): JSX.Element => (
   <div className="grid grid-cols-4 grid-rows-6 gap-8">
     <ChatAi />
+    <Games />
   </div>
 )
 
