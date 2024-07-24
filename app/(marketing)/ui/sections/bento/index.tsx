564883b2ba3449e9216fac778bfa3752297a1660
Kamilo Vasquez <killo.blanco@gmail.com>
2024-07-23 21:57:49 -0500

    ✨ Migrating from nextjs to astro

--- app/(marketing)/ui/sections/bento/index.tsx
+++
@@ -1,9 +0,0 @@
-import ChatAi from './chat-ai'
-
-const BentoShow = (): JSX.Element => (
-  <div className="grid grid-cols-4 grid-rows-6 gap-8">
-    <ChatAi />
-  </div>
-)
-
-export default BentoShow
